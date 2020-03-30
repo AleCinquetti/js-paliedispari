d766d97d306e3e5f6877c52a1aff619e9e0d6d1e
Alessandro Cinquetti <alessandro.cinquetti@gmail.com>
2020-03-30 17:53:25 +0200

    piccolo errore di battitura

--- Esercizio-2/main.js
+++ Esercizio-2/main.js
@@ -14,14 +14,13 @@ function randomNumber(a, b) {
   var number = Math.floor(Math.random() * (b - a + 1)) + a;
   return number;
 }
-
 // numeri risultato della funzione
 var firstNumber = alert(randomNumber(1, 5));
 var secondNumber = alert(randomNumber(1, 5));
 
 
 // funzione per stabilire se somma è pari o dispari
-function pariDispari(firstNumber, secondNumber) {
+function oddOrEven(firstNumber, secondNumber) {
   var result;
   var sum = firstNumber + secondNumber;
   if (sum % 2 == 0) {
@@ -32,7 +31,7 @@ function pariDispari(firstNumber, secondNumber) {
   return result;
 }
 // variabile appoggio che contiene risultato funzione (se pari o dispari)
-var oddOrEven = pariDispari(firstNumber, secondNumber);
+var oddOrEven = oddOrEven(firstNumber, secondNumber);
 
 // condizionale per output
 if (userChoice == oddOrEven) {
