b0296c25f35ba954bfee9efb3a35c453fc195897
Alessandro Cinquetti <alessandro.cinquetti@gmail.com>
2020-03-30 16:57:36 +0200

    Esercizio parola palindrome riveduto e corretto

--- Esercizio-1/main.js
+++ Esercizio-1/main.js
@@ -3,34 +3,59 @@
 // tramite una funzione (creata da te) verifico se la parola inserita è palindroma o meno;
 // dò output a utente dicendogli che ho verificato;
 
-
-// variabili globali
+// METODO: FUNZIONE CREATA DA ME
+// // variabili globali
 var word = prompt("Scrivi la tua parola per sapere se è palindroma");
-var invertedWord; //variabile di appoggio per contenere parola invertita
 
-// funzione per invertire una parola
-// Dichiaro 3 variabili interne alla funzione per "salvare" ogni step della funzione stessa
-function reverseWord(word) {
-    // SPLIT: prende singolarmente i caratteri della stringa e li inserisce in array
-    var splitWord = word.split("");
-    // REVERSE: inverte la posizione dei caratteri dell'array
-    var reverseWord = splitWord.reverse("");
-    // JOIN: unisce i caratteri dell'array per formare di nuovo una stringa
-    var joinWord = reverseWord.join("");
 
-    return joinWord;
+// creo funzione che inverta le parole
+function revertedWord(word) {
+    // creo variabile d'appoggio dove verrà inserito ogni singolo carattere della parola scelta
+     var revWord = "";
 
-    // SCRITTURA COMPATTA: return word.split("").reverse("").join("");
+     // ciclo per percorrere al contrario la parola e inserire ogni carattere all'interno della nuova variabile
+     for (var i = word.length - 1; i >= 0; i--) {
+         revWord += word[i];
+     }
+     return revWord;
 }
 
-// inserisco il "risultato" della funzione nella variabile inizializzata precedentemente
-invertedWord = reverseWord(word);
-
-console.log(invertedWord);
+var newWord = revertedWord(word);
 
 // condizionale per output
-if (word === invertedWord) {
-    alert("La parola che hai scritto è palindroma");
+if (word === newWord) {
+    alert("La parola " + word + " è palindroma");
 } else {
-    alert("La parola che hai scritto NON è palindroma");
+    alert("La parola " + word + " NON è palindroma");
 }
+
+
+
+// METODO: FUNZIONI BUILT-IN
+// // funzione per invertire una parola
+// // Dichiaro 3 variabili interne alla funzione per "salvare" ogni step della funzione stessa
+// function reverseWord(word) {
+//     // SPLIT: prende singolarmente i caratteri della stringa e li inserisce in array
+//     var splitWord = word.split("");
+//     // REVERSE: inverte la posizione dei caratteri dell'array
+//     var reverseWord = splitWord.reverse("");
+//     // JOIN: unisce i caratteri dell'array per formare di nuovo una stringa
+//     var joinWord = reverseWord.join("");
+//
+//     return joinWord;
+//
+//     // SCRITTURA COMPATTA: return word.split("").reverse("").join("");
+//
+// }
+//
+// // inserisco il "risultato" della funzione nella variabile inizializzata precedentemente
+// invertedWord = reverseWord(word);
+//
+// console.log(invertedWord);
+//
+// // condizionale per output
+// if (word === invertedWord) {
+//     alert("La parola che hai scritto è palindroma");
+// } else {
+//     alert("La parola che hai scritto NON è palindroma");
+// }
